9098791b9963cdeae8c560f235939a59da70f0e4
Nathan Drezner <nathan@drezner.xyz>
2023-01-13 09:01:39 -0500

    Simplify script tags; add a clock

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
 onload = function () {
     var parent = document.getElementById('fadein');
     var frag = document.createDocumentFragment();
-    while (parent.children.length) {
-        frag.appendChild(
-            parent.children[Math.floor(Math.random() * parent.children.length)]
-        );
+    if (parent !== null) {
+        while (parent.children.length) {
+            frag.appendChild(
+                parent.children[
+                    Math.floor(Math.random() * parent.children.length)
+                ]
+            );
+        }
+        parent.appendChild(frag);
     }
-    parent.appendChild(frag);
 };
 
 $(window).on('load', function () {
+    timeNow();
+    setInterval(timeNow, 100);
+
+    onload();
     $(window)
         .scroll(function () {
             var windowBottom = $(this).scrollTop() + $(this).innerHeight();
@@ -32,4 +40,20 @@ $(window).on('load', function () {
             });
         })
         .scroll(); //invoke scroll-handler on page-load
+
+    setInterval(timeNow, 100);
 });
+
+var time, h, m, s;
+
+function timeNow() {
+    time = new Date();
+    const zeroPad = (num, places) => String(num).padStart(places, '0');
+    h = zeroPad(time.getHours(), 2);
+    m = zeroPad(time.getMinutes(), 2);
+    s = zeroPad(time.getSeconds(), 2);
+    if (s < 10) {
+        s = '0' + s;
+    } /* we add a 0 in front of s, when it is lower than 10, because that's what most clocks display, this is for the human user rather than for any need by the computer */
+    document.getElementById('time').innerHTML = h + ':' + m + ':' + s;
+}
