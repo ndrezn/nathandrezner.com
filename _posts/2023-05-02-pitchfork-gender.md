c70437c8dfa23c709afea3fdddef62cbb0c1d523
Nathan Drezner <nathan@drezner.xyz>
2023-05-02 16:33:38 -0400

    Create sample chart

---
+++ _posts/2023-05-02-pitchfork-gender.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: Pitchfork Gender
+---
+
+{{ site.data.pitchfork_dataset.data }}
+
+<script src="https://d3js.org/d3.v4.min.js"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<div id='chart'></div>
+<script type='module' src='/js/pitchfork-gender.js'></script>
--- js/pitchfork-gender.js
+++ js/pitchfork-gender.js
@@ -1,25 +1,11 @@
+import data from '/js/pitchfork-dataset.js';
 
-
-var obj = JSON.parse({{ site.data.pitchfork_dataset.data }});
-
-console.log(mydata[0]);
-var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
+var trace = {
+    x: data['pubDate'],
+    y: data['rating'],
     mode: 'markers',
-    type: 'scatter'
+    type: 'scatter',
 };
-var trace2 = {
-    x: [2, 3, 4, 5],
-    y: [16, 5, 11, 9],
-    mode: 'lines',
-    type: 'scatter'
-};
-var trace3 = {
-    x: [1, 2, 3, 4],
-    y: [12, 9, 15, 12],
-    mode: 'lines+markers',
-    type: 'scatter'
-};
-var data = [trace1, trace2, trace3];
-Plotly.newPlot('myDiv', data);
+var charts = [trace];
+
+Plotly.newPlot('chart', charts);
