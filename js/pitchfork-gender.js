137d1abfeda72571404085a9b4adcb81c0a9b0f2
Nathan Drezner <nathan@drezner.xyz>
2023-05-02 17:03:52 -0400

    Use promise to generate figure

--- js/pitchfork-gender.js
+++ js/pitchfork-gender.js
@@ -1,11 +1,20 @@
-import data from '/js/pitchfork-dataset.js';
+fetch('/data/pitchfork_dataset.json')
+    .then((response) => response.json())
+    .then((pitchfork_data) => {
+        let scatter_chart = {
+            data: [
+                {
+                    x: pitchfork_data['pubDate'],
+                    y: pitchfork_data['rating'],
+                    mode: 'markers',
+                    type: 'scatter',
+                },
+            ],
+            layout: {
+                paper_bgcolor: 'rgba(0,0,0,0)',
+                plot_bgcolor: 'rgba(0,0,0,0)',
+            },
+        };
 
-var trace = {
-    x: data['pubDate'],
-    y: data['rating'],
-    mode: 'markers',
-    type: 'scatter',
-};
-var charts = [trace];
-
-Plotly.newPlot('chart', charts);
+        Plotly.newPlot('chart', scatter_chart.data, scatter_chart.layout);
+    });
