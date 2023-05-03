cc32ac3aeedd3af9353e551b1b6580035bf3387d
Nathan Drezner <nathan@drezner.xyz>
2023-05-03 10:41:03 -0400

    Update figures

--- _posts/2023-05-02-pitchfork-gender.md
+++ _posts/2023-05-02-pitchfork-gender.md
@@ -8,8 +8,10 @@ title: Pitchfork Gender
 
 Words words words
 
-<div id='chart'></div>
+<div id='scatter-summary'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Every review ever given by Pitchfork -- select specific categories by clicking on the key on the right, and hover on points to see more information.</small></div>
 
-Words words words
+<div id='box-plot'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Year-by-year score aggregations. Fewer extreme low scores are given out over time, but the average score and score distribution is largely similar year over year. The real average hovers closer to 7.4, not <a href='https://www.theonion.com/pitchfork-gives-music-6-8-1819569318'>The Onion's 6.8</a></small></div>
 
 <script type='module' src='/js/pitchfork-gender.js'></script>
--- js/pitchfork-gender.js
+++ js/pitchfork-gender.js
@@ -1,42 +1,88 @@
+const generate_scatter = (dataset) => {
+    let scatter_chart = {
+        data: [
+            {
+                x: dataset['pubDate'],
+                y: dataset['rating'],
+                text: dataset['album'],
+                mode: 'markers',
+                type: 'scatter',
+                transforms: [
+                    {
+                        type: 'groupby',
+                        groups: dataset['artist_gender'],
+                    },
+                ],
+                hovertemplate:
+                    '<b>%{text}</b><br><br>' +
+                    '%{yaxis.title.text}: %{y}<br>' +
+                    '%{xaxis.title.text}: %{x}<br>' +
+                    '<extra></extra>',
+            },
+        ],
+        layout: {
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            margin: {t: 30, b: 30, l: 30, r: 2},
+            hovermode: 'closest',
+            xaxis: {
+                title: 'Review date',
+                zeroline: false,
+            },
+            yaxis: {
+                title: 'Score',
+                zeroline: false,
+            },
+        },
+    };
+
+    Plotly.newPlot('scatter-summary', scatter_chart.data, scatter_chart.layout);
+};
+
+const generate_box = (dataset) => {
+    let box_chart = {
+        data: [
+            {
+                x: dataset['pubYear'],
+                y: dataset['rating'],
+                text: dataset['album'],
+                type: 'box',
+                hovertemplate:
+                    '<b>%{text}</b><br><br>' +
+                    '%{yaxis.title.text}: %{y}<br>' +
+                    '%{xaxis.title.text}: %{x}<br>' +
+                    '<extra></extra>',
+            },
+        ],
+        layout: {
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            margin: {t: 30, b: 30, l: 30, r: 2},
+            hovermode: 'closest',
+            xaxis: {
+                title: 'Review year',
+                zeroline: false,
+            },
+            yaxis: {
+                title: 'Score',
+                zeroline: false,
+            },
+        },
+    };
+
+    Plotly.newPlot('box-plot', box_chart.data, box_chart.layout);
+};
+
+const get_year = (date) => {
+    return date.slice(0, 4);
+};
+
 fetch('/data/pitchfork_dataset.json')
     .then((response) => response.json())
     .then((pitchfork_data) => {
-        let scatter_chart = {
-            data: [
-                {
-                    x: pitchfork_data['pubDate'],
-                    y: pitchfork_data['rating'],
-                    text: pitchfork_data['album'],
-                    mode: 'markers',
-                    type: 'scatter',
-                    transforms: [
-                        {
-                            type: 'groupby',
-                            groups: pitchfork_data['artist_gender'],
-                        },
-                    ],
-                    hovertemplate:
-                        '<b>%{text}</b><br><br>' +
-                        '%{yaxis.title.text}: %{y}<br>' +
-                        '%{xaxis.title.text}: %{x}<br>' +
-                        '<extra></extra>',
-                },
-            ],
-            layout: {
-                paper_bgcolor: 'rgba(0,0,0,0)',
-                plot_bgcolor: 'rgba(0,0,0,0)',
-                margin: {t: 30, b: 30, l: 30, r: 2},
-                hovermode: 'closest',
-                xaxis: {
-                    title: 'Review date',
-                    zeroline: false,
-                },
-                yaxis: {
-                    title: 'Score',
-                    zeroline: false,
-                },
-            },
-        };
+        generate_scatter(pitchfork_data);
 
-        Plotly.newPlot('chart', scatter_chart.data, scatter_chart.layout);
+        pitchfork_data['pubYear'] = pitchfork_data['pubDate'].map(get_year);
+
+        generate_box(pitchfork_data);
     });
