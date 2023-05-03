f19e5436f17e25b2cd2d5586326d58c854c92c3d
Nathan Drezner <nathan@drezner.xyz>
2023-05-03 15:14:53 -0400

    Add more figures

--- _posts/2023-05-02-pitchfork-gender.md
+++ _posts/2023-05-02-pitchfork-gender.md
@@ -8,10 +8,25 @@ title: Pitchfork Gender
 
 Words words words
 
-<div id='scatter-summary'></div>
+<div id='scatter-summary' style='min-height: 500px'></div>
 <div style="text-align:center; margin: 0 auto;"><small >Every review ever given by Pitchfork -- select specific categories by clicking on the key on the right, and hover on points to see more information.</small></div>
 
-<div id='box-plot'></div>
-<div style="text-align:center; margin: 0 auto;"><small >Year-by-year score aggregations. Fewer extreme low scores are given out over time, but the average score and score distribution is largely similar year over year. The real average hovers closer to 7.4, not <a href='https://www.theonion.com/pitchfork-gives-music-6-8-1819569318'>The Onion's 6.8</a></small></div>
+<div id='box-plot' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Year-by-year score aggregations. Fewer extreme low scores are given out over time, and the middle 50% of scores narrows from a span of 1.8 to 0.7. The real average hovers closer to 7.4, not <a href='https://www.theonion.com/pitchfork-gives-music-6-8-1819569318'><i>The Onion</i>'s 6.8.</a></small></div>
+
+<div id='stacked-bar' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Count of reviews over year, grouped by gender. Pitchfork has been surprisingly consistent in its editorial output since its inception, and there are far more women artists represented over time.</small></div>
+
+<div id='bnm-scatter-summary' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >All Best New Music reviews.</small></div>
+
+<div id='bnm-box-plot' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Aggregating the scores for best new music reviews.</small></div>
+
+<div id='bnm-stacked-bar' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Best new music reviews as grouped by artist gender and count.</small></div>
+
+<div id='bnm-stacked-bar-contributor' style='min-height: 500px'></div>
+<div style="text-align:center; margin: 0 auto;"><small >Does the contributor's title play a role in whether they have a BNM byline?</small></div>
 
 <script type='module' src='/js/pitchfork-gender.js'></script>
--- js/pitchfork-gender.js
+++ js/pitchfork-gender.js
@@ -1,10 +1,10 @@
-const generate_scatter = (dataset) => {
+const generate_scatter = (dataset, id) => {
     let scatter_chart = {
         data: [
             {
                 x: dataset['pubDate'],
                 y: dataset['rating'],
-                text: dataset['album'],
+                text: dataset['hovertext'],
                 mode: 'markers',
                 type: 'scatter',
                 transforms: [
@@ -14,7 +14,7 @@ const generate_scatter = (dataset) => {
                     },
                 ],
                 hovertemplate:
-                    '<b>%{text}</b><br><br>' +
+                    '%{text}<br><br>' +
                     '%{yaxis.title.text}: %{y}<br>' +
                     '%{xaxis.title.text}: %{x}<br>' +
                     '<extra></extra>',
@@ -30,25 +30,25 @@ const generate_scatter = (dataset) => {
                 zeroline: false,
             },
             yaxis: {
-                title: 'Score',
+                title: 'Rating',
                 zeroline: false,
             },
         },
     };
 
-    Plotly.newPlot('scatter-summary', scatter_chart.data, scatter_chart.layout);
+    Plotly.newPlot(id, scatter_chart.data, scatter_chart.layout);
 };
 
-const generate_box = (dataset) => {
+const generate_box = (dataset, id) => {
     let box_chart = {
         data: [
             {
                 x: dataset['pubYear'],
                 y: dataset['rating'],
-                text: dataset['album'],
+                text: dataset['hovertext'],
                 type: 'box',
                 hovertemplate:
-                    '<b>%{text}</b><br><br>' +
+                    '%{text}<br><br>' +
                     '%{yaxis.title.text}: %{y}<br>' +
                     '%{xaxis.title.text}: %{x}<br>' +
                     '<extra></extra>',
@@ -64,25 +64,113 @@ const generate_box = (dataset) => {
                 zeroline: false,
             },
             yaxis: {
-                title: 'Score',
+                title: 'Rating',
                 zeroline: false,
             },
         },
     };
 
-    Plotly.newPlot('box-plot', box_chart.data, box_chart.layout);
+    Plotly.newPlot(id, box_chart.data, box_chart.layout);
 };
 
-const get_year = (date) => {
-    return date.slice(0, 4);
+const generate_stacked_bar = (dataset, id, groupby_column) => {
+    let chart = {
+        data: [
+            {
+                x: dataset['pubYear'],
+                y: dataset['rating'],
+                type: 'bar',
+                text: dataset['artist_gender'],
+                hovertemplate:
+                    '%{text}<br><br>' +
+                    '%{yaxis.title.text}: %{y}<br>' +
+                    '%{xaxis.title.text}: %{x}<br>' +
+                    '<extra></extra>',
+                transforms: [
+                    {
+                        type: 'aggregate',
+                        groups: dataset['pubYear'],
+                        aggregations: [{target: 'y', func: 'count'}],
+                    },
+                    {
+                        type: 'groupby',
+                        groups: dataset[groupby_column],
+                    },
+                ],
+            },
+        ],
+        layout: {
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
+            margin: {t: 30, b: 30, l: 50, r: 2},
+            hovermode: 'closest',
+            barmode: 'stack',
+            xaxis: {
+                title: 'Review year',
+                zeroline: false,
+            },
+            yaxis: {
+                title: 'Count',
+                zeroline: false,
+            },
+        },
+    };
+
+    Plotly.newPlot(id, chart.data, chart.layout);
 };
+
+function getAverage(str) {
+    const nums = str.match(/\d+(\.\d+)?/g).map(Number);
+    const sum = nums.reduce((acc, val) => acc + val, 0);
+    const avg = sum / nums.length;
+    return avg;
+}
+
+function filterAll(filterCol, dataset, condition) {
+    let filtered_dataset = Object.keys(dataset).reduce(
+        (acc, key) => ({...acc, [key]: []}),
+        {}
+    );
+    for (let i = 0; i < filterCol.length; i++) {
+        if (condition(filterCol[i])) {
+            for (let key in dataset) {
+                filtered_dataset[key].push(dataset[key][i]);
+            }
+        }
+    }
+
+    return filtered_dataset;
+}
 
 fetch('/data/pitchfork_dataset.json')
     .then((response) => response.json())
     .then((pitchfork_data) => {
-        generate_scatter(pitchfork_data);
+        // Data transformations & additional columns
+        pitchfork_data['hovertext'] = pitchfork_data['album'].map(
+            (e, i) => '<i>' + e + '</i> (' + pitchfork_data['artist'][i] + ')'
+        );
+        pitchfork_data['pubYear'] = pitchfork_data['pubDate'].map((date, i) =>
+            date.slice(0, 4)
+        );
 
-        pitchfork_data['pubYear'] = pitchfork_data['pubDate'].map(get_year);
+        // Generate our figures
+        generate_scatter(pitchfork_data, 'scatter-summary');
+        generate_box(pitchfork_data, 'box-plot');
+        generate_stacked_bar(pitchfork_data, 'stacked-bar', 'artist_gender');
 
-        generate_box(pitchfork_data);
+        // best new music
+        const {bnm, ...rest} = pitchfork_data;
+        let bnm_reviews = filterAll(bnm, rest, Boolean);
+        console.log(bnm_reviews.length);
+
+        bnm_reviews['rating'] = bnm_reviews['rating'].map(getAverage);
+        generate_box(bnm_reviews, 'bnm-box-plot');
+        generate_scatter(bnm_reviews, 'bnm-scatter-summary');
+
+        generate_stacked_bar(bnm_reviews, 'bnm-stacked-bar', 'artist_gender');
+        generate_stacked_bar(
+            bnm_reviews,
+            'bnm-stacked-bar-contributor',
+            'author_title'
+        );
     });
