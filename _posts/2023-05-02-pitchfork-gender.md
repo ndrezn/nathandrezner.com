995a7d607529c746f947d0ec362b4569c127b94d
Nathan Drezner <nathan@drezner.xyz>
2023-05-03 09:36:59 -0400

    Improve figure

--- _posts/2023-05-02-pitchfork-gender.md
+++ _posts/2023-05-02-pitchfork-gender.md
@@ -3,9 +3,13 @@ layout: post
 title: Pitchfork Gender
 ---
 
-{{ site.data.pitchfork_dataset.data }}
-
 <script src="https://d3js.org/d3.v4.min.js"></script>
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+Words words words
+
 <div id='chart'></div>
+
+Words words words
+
 <script type='module' src='/js/pitchfork-gender.js'></script>
--- js/pitchfork-gender.js
+++ js/pitchfork-gender.js
@@ -6,13 +6,35 @@ fetch('/data/pitchfork_dataset.json')
                 {
                     x: pitchfork_data['pubDate'],
                     y: pitchfork_data['rating'],
+                    text: pitchfork_data['album'],
                     mode: 'markers',
                     type: 'scatter',
+                    transforms: [
+                        {
+                            type: 'groupby',
+                            groups: pitchfork_data['artist_gender'],
+                        },
+                    ],
+                    hovertemplate:
+                        '<b>%{text}</b><br><br>' +
+                        '%{yaxis.title.text}: %{y}<br>' +
+                        '%{xaxis.title.text}: %{x}<br>' +
+                        '<extra></extra>',
                 },
             ],
             layout: {
                 paper_bgcolor: 'rgba(0,0,0,0)',
                 plot_bgcolor: 'rgba(0,0,0,0)',
+                margin: {t: 30, b: 30, l: 30, r: 2},
+                hovermode: 'closest',
+                xaxis: {
+                    title: 'Review date',
+                    zeroline: false,
+                },
+                yaxis: {
+                    title: 'Score',
+                    zeroline: false,
+                },
             },
         };
 
